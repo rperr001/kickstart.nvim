511b665deec45f5e0321dab9e85dc7bddd0b7916
Robert L Perry <robertlperry@gmail.com>
2024-04-21 02:16:29 -0400

    Adding <leader> h and v for terminal

--- lua/kickstart/plugins/terminal.lua
+++ lua/kickstart/plugins/terminal.lua
@@ -4,9 +4,40 @@
 
 return {
   {
-    'NvChad/nvterm',
+    'zbirenbaum/nvterm',
     config = function()
-      require('nvterm').setup()
+      require('nvterm').setup {
+        terminals = {
+          shell = vim.o.shell,
+          list = {},
+          type_opts = {
+            float = {
+              relative = 'editor',
+              row = 0.3,
+              col = 0.25,
+              width = 0.5,
+              height = 0.4,
+              border = 'single',
+            },
+            horizontal = { location = 'rightbelow', split_ratio = 0.3 },
+            vertical = { location = 'rightbelow', split_ratio = 0.5 },
+          },
+        },
+        behavior = {
+          autoclose_on_quit = {
+            enabled = false,
+            confirm = true,
+          },
+          close_on_exit = true,
+          auto_insert = true,
+        },
+      }
+      vim.keymap.set('n', '<leader>h', function()
+        require('nvterm.terminal').new 'horizontal'
+      end, { desc = 'horizontal terminal' })
+      vim.keymap.set('n', '<leader>v', function()
+        require('nvterm.terminal').new 'vertical'
+      end, { desc = 'vertical terminal' })
     end,
   },
 }
